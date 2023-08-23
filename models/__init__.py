099bfe29fc67437b5bb90ba64121ffe72ec4fbf3
reckafella <ethanmuthoniw6@gmail.com>
2023-08-23 17:47:14 +0300

    Update AUTHORS file

--- models/__init__.py
+++ models/__init__.py
@@ -8,4 +8,4 @@ if getenv("HBNB_TYPE_STORAGE") == 'db':
     storage = DBStorage()
 else:
     storage = FileStorage()
-storage.reload():
+storage.reload()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module implementing tools to connect with a MySQL database """
+
+
+class DBStorage():
+    """ Class managing data storage in a MySQL database """
+    pass
