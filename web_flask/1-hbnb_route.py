cf4cf0782920e59ab7dea6797cb0f8ef96921d19
reckafella <ethanmuthoniw6@gmail.com>
2023-09-25 01:44:36 +0300

    Add 1-hbnb_route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Module to run the Flask web app on 0.0.0.0:5000 and print "Hello HBNB!"
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
