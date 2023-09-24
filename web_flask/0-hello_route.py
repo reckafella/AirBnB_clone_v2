0cc1dcfeaa83a312cb6a6870debaa6b93fe07a09
reckafella <ethanmuthoniw6@gmail.com>
2023-09-25 01:28:53 +0300

    Update 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-''' Module to run the Flask web app on 0.0.0.0:5000 '''
+'''
+Module to run the Flask web app on 0.0.0.0:5000 and print "Hello HBNB!"
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	return "<p>Hello, HBNB!</p>"
-@app.run(host='0.0.0.0', port=5000)
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
