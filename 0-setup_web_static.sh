405563c627585032a410313a5f07c5dda8da39d6
reckafella <ethanmuthoniw6@gmail.com>
2023-09-28 15:14:54 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,7 +21,6 @@ sudo ln -s -f /data/web_static/releases/test /data/web_static/current
 # Give ownership of "/data/" to user "ubuntu" and group "ubuntu"
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve content of /data/web_static_current/ to
-# hbnb_static
+# Update Nginx configuration to serve content of /data/web_static/current/ to /hbnb_static
 sudo sed -i "/listen 80 default_server;/a \\\tlocation \/hbnb_static \{\n\\t\\talias \/data\/web_static\/current\/;\n\\t\}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
