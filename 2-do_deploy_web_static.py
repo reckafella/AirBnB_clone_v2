2870c1259b41641c740b8a3ad68731e1ffa3049b
Reckafella <ethanmuthoni@gmail.com>
2023-10-08 10:02:14 -0700

    update files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,6 +25,8 @@ def do_deploy(archive_path):
 
         run('mkdir -p {}'.format(release_path))
         run('tar -zxvf /tmp/{} -C {}'.format(file_name, release_path))
+        run('mv {}/web_static/* {}'.format(release_path, release_path))
+        run('rm -rf {}/web_static/'.format(release_path))
 
         run('rm /tmp/{}'.format(file_name))
 
