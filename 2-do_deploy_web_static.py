082ecbad0a4a82752adfc9ed4b847f36f7885051
reckafella <ethanmuthoniw6@gmail.com>
2023-10-09 02:57:34 +0300

    Wueh. I can't figure out issues here

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,33 +5,13 @@ archive to your web servers, using the function do_deploy
 '''
 from fabric.api import env
 from fabric.api import run
-from fabric.api import local
 from fabric.api import put
-from datetime import datetime
 from os import path
 
 
 env.hosts = ['100.26.241.94', '35.168.1.10']
-
-
-def do_pack():
-    '''
-    generates a .tgz archive from the contents of the web_static folder
-    '''
-    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "web_static_{}.tgz".format(time_stamp)
-
-    # create versions folders if it does not exist
-    local("mkdir -p versions")
-    full_path_to_archive = "versions/{}".format(file_name)
-
-    # Create archive of web_static
-    archive = local("tar -czvf {} web_static".format(full_path_to_archive))
-
-    if archive.succeeded:
-        return (full_path_to_archive)
-    else:
-        return (None)
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -40,26 +20,20 @@ def do_deploy(archive_path):
     '''
     if not path.exists(archive_path):
         return False
-
     try:
         file_name = archive_path.split('/')[-1]
         folder_name = file_name.split('.')[0]
         release_path = '/data/web_static/releases/{}'.format(folder_name)
-
-        put(local_path=archive_path, remote_path='/tmp/')
-
-        run('mkdir -p {}'.format(release_path))
-        run('tar -zxvf /tmp/{} -C {}'.format(file_name, release_path))
-        run('mv {}/web_static/* {}'.format(release_path, release_path))
-        run('rm -rf {}/web_static/'.format(release_path))
-
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}/'.format(release_path))
+        run('tar -zxvf /tmp/{} -C {}/'.format(file_name, release_path))
         run('rm /tmp/{}'.format(file_name))
-
+        run('mv {}/web_static/* {}/'.format(release_path, release_path))
+        run('rm -rf {}/web_static'.format(release_path))
         current_link = '/data/web_static/current'
         run('rm -f {}'.format(current_link))
-
         run('ln -s {} {}'.format(release_path, current_link))
-
-        return True
-    except Exception as e:
+    except:
         return False
+    print("New version deployed!")
+    return True
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,6 +11,8 @@ from os import path
 
 
 env.hosts = ['100.26.241.94', '35.168.1.10']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
@@ -39,27 +41,23 @@ def do_deploy(archive_path):
     '''
     if not path.exists(archive_path):
         return False
-
     try:
         file_name = archive_path.split('/')[-1]
         folder_name = file_name.split('.')[0]
         release_path = '/data/web_static/releases/{}'.format(folder_name)
-
         put(archive_path, '/tmp/')
-
-        run('mkdir -p {}'.format(release_path))
+        run('mkdir -p {}/'.format(release_path))
         run('tar -zxvf /tmp/{} -C {}/'.format(file_name, release_path))
         run('rm /tmp/{}'.format(file_name))
-
-        run('mv {}/web_static/* {}'.format(release_path, release_path))
+        run('mv {}/web_static/* {}/'.format(release_path, release_path))
         run('rm -rf {}/web_static'.format(release_path))
-
         current_link = '/data/web_static/current'
         run('rm -f {}'.format(current_link))
         run('ln -s {} {}'.format(release_path, current_link))
-        return True
-    except Exception as e:
+    except:
         return False
+    print("New version deployed!")
+    return True
 
 
 def deploy():
