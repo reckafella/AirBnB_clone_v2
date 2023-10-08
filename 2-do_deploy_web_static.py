5d7ecf85465dc135f4c6dd5e9e36eeccc0681805
reckafella <ethanmuthoniw6@gmail.com>
2023-10-08 18:49:01 +0300

    Update 2-do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,21 +21,16 @@ def do_deploy(archive_path):
         folder_name = file_name.split('.')[0]
         release_path = '/data/web_static/releases/{}'.format(folder_name)
 
-        # Upload archive
         put(local_path=archive_path, remote_path='/tmp/')
 
-        # Extract archive
         run('mkdir -p {}'.format(release_path))
         run('tar -zxvf /tmp/{} -C {}'.format(file_name, release_path))
 
-        # delete archive from web servers
         run('rm /tmp/{}'.format(file_name))
 
-        #  delete existing symbolic link to /data/web_static/current
         current_link = '/data/web_static/current'
         run('rm -f {}'.format(current_link))
 
-        # create new symbolic link
         run('ln -s {} {}'.format(release_path, current_link))
 
         return True
