1025edf69c24af2298ff57677c96163176559ba6
Reckafella <ethanmuthoni@gmail.com>
2023-11-23 21:21:50 +0300

    add 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+Module to run the Flask web app on 0.0.0.0:5000
+'''
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_c(text=None):
+    if text:
+        return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def print_python(text='is cool'):
+    ''' returns a message when /python is called '''
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def print_number(n):
+    ''' display n is a number if it is integer '''
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
